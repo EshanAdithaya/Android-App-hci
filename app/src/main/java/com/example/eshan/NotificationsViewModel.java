ec40ed939f33c2bda339c2c8ca57ddd036524a49
EshanAdithaya <87906517+EshanAdithaya@users.noreply.github.com>
2024-06-17 21:14:52 +0530

    test notifications

---
+++ app/src/main/java/com/example/eshan/NotificationsViewModel.java
@@ -0,0 +1,19 @@
+package com.example.eshan;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class NotificationsViewModel extends ViewModel {
+
+    private final MutableLiveData<String> text;
+
+    public NotificationsViewModel() {
+        text = new MutableLiveData<>();
+        text.setValue("This is notifications fragment");
+    }
+
+    public LiveData<String> getText() {
+        return text;
+    }
+}
